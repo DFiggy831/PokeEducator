171b923caec0de41a105d030b7dac14e5f90a82e
Daniel Figueroa <dafigueroa@csumb.edu>
2020-12-09 17:37:15 -0800

    Last portion of Pokemon Information

---
+++ pokemon_info3.py
@@ -0,0 +1,156 @@
+pokemon_info = [
+    {
+        "name" : "Amaura",
+        "type1" : "Rock",
+        "type2" : "Ice",
+        "strengths" : ["Normal", "Flying", "Poison", "Ice"],
+        "image_id" : "698"
+    },
+    {
+        "name" : "Aurorus",
+        "type1" : "Rock",
+        "type2" : "Ice",
+        "strengths" : ["Normal", "Flying", "Poison", "Ice"],
+        "image_id" : "699"
+    },
+    {
+        "name" : "Sylveon",
+        "type1" : "Fairy",
+        "type2" : "Neutral",
+        "strengths" : ["Dragon", "Fighting", "Bug", "Dark"],
+        "image_id" : "700"
+    },
+    {
+        "name" : "Hawlucha",
+        "type1" : "Fighting",
+        "type2" : "Flying",
+        "strengths" : ["Ground", "Bug", "Dark", "Fighting", "Grass"],
+        "image_id" : "701"
+    },
+    {
+        "name" : "Dedenne",
+        "type1" : "Electric",
+        "type2" : "Fairy",
+        "strengths" : ["Dragon", "Flying", "Electric", "Fighting", "Bug", "Dark"],
+        "image_id" : "702"
+    },
+    {
+        "name" : "Carbink",
+        "type1" : "Rock",
+        "type2" : "Fairy",
+        "strengths" : ["Dragon", "Normal", "Flying", "Fire", "Bug", "Dark"],
+        "image_id" : "703"
+    },
+    {
+        "name" : "Goomy",
+        "type1" : "Dragon",
+        "type2" : "Neutral",
+        "strengths" : ["Fire", "Water", "Grass", "Electric"],
+        "image_id" : "704"
+    },
+    {
+        "name" : "Sliggoo",
+        "type1" : "Dragon",
+        "type2" : "Neutral",
+        "strengths" : ["Fire", "Water", "Grass", "Electric"],
+        "image_id" : "705"
+    },
+    {
+        "name" : "Goodra",
+        "type1" : "Dragon",
+        "type2" : "Neutral",
+        "strengths" : ["Fire", "Water", "Grass", "Electric"],
+        "image_id" : "706"
+    },
+    {
+        "name" : "Klefki",
+        "type1" : "Steel",
+        "type2" : "Fairy",
+        "strengths" : ["Dragon", "Bug", "Poison", "Normal", "Flying", "Rock", "Grass", "Psychic", "Ice", "Fairy", "Dark"],
+        "image_id" : "707"
+    },
+    {
+        "name" : "Phantump",
+        "type1" : "Ghost",
+        "type2" : "Grass",
+        "strengths" : ["Normal", "Fighting", "Ground", "Water", "Grass", "Electric"],
+        "image_id" : "708"
+    },
+    {
+        "name" : "Trevenant",
+        "type1" : "Ghost",
+        "type2" : "Grass",
+        "strengths" : ["Normal", "Fighting", "Ground", "Water", "Grass", "Electric"],
+        "image_id" : "709"
+    },
+    {
+        "name" : "Pumpkaboo",
+        "type1" : "Ghost",
+        "type2" : "Grass",
+        "strengths" : ["Normal", "Fighting", "Ground", "Water", "Grass", "Electric"],
+        "image_id" : "710"
+    },
+    {
+        "name" : "Gourgeist",
+        "type1" : "Ghost",
+        "type2" : "Grass",
+        "strengths" : ["Normal", "Fighting", "Ground", "Water", "Grass", "Electric"],
+        "image_id" : "711"
+    },
+    {
+        "name" : "Bergmite",
+        "type1" : "Ice",
+        "type2" : "Neutral",
+        "strengths" : ["Ice"],
+        "image_id" : "712"
+    },
+    {
+        "name" : "Avalugg",
+        "type1" : "Ice",
+        "type2" : "Neutral",
+        "strengths" : ["Ice"],
+        "image_id" : "713"
+    },
+    {
+        "name" : "Noibat",
+        "type1" : "Flying",
+        "type2" : "Dragon",
+        "strengths" : ["Ground", "Grass", "Fighting", "Bug", "Fire", "Water"],
+        "image_id" : "714"
+    },
+    {
+        "name" : "Noivern",
+        "type1" : "Flying",
+        "type2" : "Dragon",
+        "strengths" : ["Ground", "Grass", "Fighting", "Bug", "Fire", "Water"],
+        "image_id" : "715"
+    },
+    {
+        "name" : "Xerneas",
+        "type1" : "Fairy",
+        "type2" : "Neutral",
+        "strengths" : ["Dragon", "Fighting", "Bug", "Dark"],
+        "image_id" : "716"
+    },
+    {
+        "name" : "Yveltal",
+        "type1" : "Dark",
+        "type2" : "Flying",
+        "strengths" : ["Psychic", "Ground", "Ghost", "Dark", "Grass"],
+        "image_id" : "717"
+    },
+    {
+        "name" : "Zygarde",
+        "type1" : "Dragon",
+        "type2" : "Ground",
+        "strengths" : ["Electric", "Fire", "Poison", "Rock"],
+        "image_id" : "718"
+    },
+    {
+        "name" : "Diancie",
+        "type1" : "Rock",
+        "type2" : "Fairy",
+        "strengths" : ["Dragon", "Normal", "Flying", "Fire", "Bug", "Dark"],
+        "image_id" : "719"
+    },
+]
