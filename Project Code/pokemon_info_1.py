4c41c9cfe159bf138c22a20a879d214d4b7992e5
nyra-23 <nyrausi@gmail.com>
2020-12-09 17:24:08 -0800

    Providing with images folder with a draft of the pokemon_info_1.

---
+++ Project Code/pokemon_info_1.py
@@ -0,0 +1,163 @@
+pokemon_info = [
+    {
+        "name" : "Chespin",
+        "type1" : "Grass",
+        "type2" : "Neutral",
+        "strengths" : ["Ground", "Water", "Grass", "Electric"],
+        "image_id" : "650"
+    },
+    {
+        "name" : "Quilladin",
+        "type1" : "Grass",
+        "type2" : "Neutral",
+        "strengths" : ["Ground", "Water", "Grass", "Electric"],
+        "image_id" : "651"
+    },
+    {
+        "name" : "Chesnaught",
+        "type1" : "Grass",
+        "type2" : "Fighting",
+        "strengths" : ["Ground", "Water", "Grass", "Electric", "Rock", "Dark"],
+        "image_id" : "652"
+    },
+    {
+        "name" : "Fennekin",
+        "type1" : "Fire",
+        "type2" : "Neutral",
+        "strengths" : ["Bug", "Steel", "Fire", "Grass", "Ice", "Fairy"],
+        "image_id" : "653"
+    },
+    {
+        "name" : "Braixen",
+        "type1" : "Fire",
+        "type2" : "Neutral",
+        "strengths" : ["Bug", "Steel", "Fire", "Grass", "Ice", "Fairy"],
+        "image_id" : "654"
+    },
+    {
+        "name" : "Delphox",
+        "type1" : "Fire",
+        "type2" : "Psychic",
+        "strengths" : ["Steel", "Fire", "Grass", "Ice", "Fairy", "Fighting", "Psychic"],
+        "image_id" : "655"
+    },
+    {
+        "name" : "Froakie",
+        "type1" : "Water",
+        "type2" : "Neutral",
+        "strengths" : ["Steel", "Fire", "Water", "Ice"],
+        "image_id" : "656"
+    },
+    {
+        "name" : "Frogadier",
+        "type1" : "Water",
+        "type2" : "Neutral",
+        "strengths" : ["Steel", "Fire", "Water", "Ice"],
+        "image_id" : "657"
+    },
+    {
+        "name" : "Greninja",
+        "type1" : "Water",
+        "type2" : "Dark",
+        "strengths" : ["Psychic", "Steel", "Fire", "Water", "Ice", "Ghost", "Dark"],
+        "image_id" : "658"
+    },
+    {
+        "name" : "Bunnelby",
+        "type1" : "Normal",
+        "type2" : "Neutral",
+        "strengths" : ["Ghost"],
+        "image_id" : "659"
+    },
+    {
+        "name" : "Diggersby",
+        "type1" : "Normal",
+        "type2" : "Ground",
+        "strengths" : ["Ghost", "Electric", "Poison", "Rock"],
+        "image_id" : "660"
+    },
+    {
+        "name" : "Fletchling",
+        "type1" : "Normal",
+        "type2" : "Flying",
+        "strengths" : ["Ghost", "Ground", "Bug", "Grass"],
+        "image_id" : "661"
+    },
+    {
+        "name" : "Fletchinder",
+        "type1" : "Fire",
+        "type2" : "Flying",
+        "strengths" : ["Ground", "Bug", "Grass", "Steel", "Fire", "Fairy", "Fighting"],
+        "image_id" : "662"
+    },
+    {
+        "name" : "Talonflame",
+        "type1" : "Fire",
+        "type2" : "Flying",
+        "strengths" : ["Ground", "Bug", "Grass", "Steel", "Fire", "Fairy", "Fighting"],
+        "image_id" : "663"
+    },
+    {
+        "name" : "Scatterbug",
+        "type1" : "Bug",
+        "type2" : "Neutral",
+        "strengths" : ["Fighting", "Ground", "Grass"],
+        "image_id" : "664"
+    },
+    {
+        "name" : "Spewpa",
+        "type1" : "Bug",
+        "type2" : "Neutral",
+        "strengths" : ["Fighting", "Ground", "Grass"],
+        "image_id" : "665"
+    },
+    {
+        "name" : "Vivillon",
+        "type1" : "Bug",
+        "type2" : "Flying",
+        "strengths" : ["Ground", "Fighting", "Grass", "Bug"],
+        "image_id" : "666"
+    },
+    {
+        "name" : "Litleo",
+        "type1" : "Fire",
+        "type2" : "Normal",
+        "strengths" : ["Ghost", "Bug", "Steel", "Fire", "Grass", "Ice", "Fairy"],
+        "image_id" : "667"
+    },
+    {
+        "name" : "Pyroar",
+        "type1" : "Fire",
+        "type2" : "Normal",
+        "strengths" : ["Ghost", "Bug", "Steel", "Fire", "Grass", "Ice", "Fairy"],
+        "image_id" : "668"
+    },
+    {
+        "name" : "Flabebe",
+        "type1" : "Fairy",
+        "type2" : "Neutral",
+        "strengths" : ["Dragon", "Fighting", "Bug", "Dark"],
+        "image_id" : "669"
+    },
+    {
+        "name" : "Floette",
+        "type1" : "Fairy",
+        "type2" : "Neutral",
+        "strengths" : ["Dragon", "Fighting", "Bug", "Dark"],
+        "image_id" : "670"
+    },
+    {
+        "name" : "Florges",
+        "type1" : "Fairy",
+        "type2" : "Neutral",
+        "strengths" : ["Dragon", "Fighting", "Bug", "Dark"],
+        "image_id" : "671"
+    },
+    {
+        "name" : "Skiddo",
+        "type1" : "Grass",
+        "type2" : "Neutral",
+        "strengths" : ["Ground", "Water", "Grass", "Electric"],
+        "image_id" : "672"
+    },
+]
